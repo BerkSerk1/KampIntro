34313524b89e658f72716c2620a069d69903f89f
BerkSerk1 <76884554+BerkSerk1@users.noreply.github.com>
2021-01-19 10:02:27 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kurs = new string[] {"Java","Python","C#"};
+
+            for (int i = 0; i < kurs.Length; i++)
+            {
+                Console.WriteLine(kurs[i]);
+            }
+            Console.WriteLine("------------------------------");
+            
+            foreach (string kurslar in kurs)
+            {
+                Console.WriteLine(kurslar);
+            }
+        }
+    }
+}
